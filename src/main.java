bdef0feab4550d909ab8043cf55b411bbd92149c
M7-0 <mohdkamil004@gmail.com>
2026-07-18 05:22:28 +0300

    setting up the time

--- src/main.java
+++ src/main.java
@@ -1,5 +1,29 @@
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args) {
+        // JAVA ALARM CLOCK:
+        Scanner kb = new Scanner(System.in);
 
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        LocalTime alarmTime = null;
+
+        while (alarmTime == null) {
+            try {
+                System.out.print("Enter the alarm time (HH:MM:SS): ");
+                String inputTime = kb.nextLine();
+                alarmTime = LocalTime.parse(inputTime, formatter);
+                System.out.println("Alarm set for " + alarmTime);
+
+            } catch (DateTimeParseException e) {
+                System.out.println("Invalid format. use a correct format of (HH:MM:SS)");
+
+            }
+        }
+
+        kb.close();
     }
 }
